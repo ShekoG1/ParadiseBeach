ba0d9cb2dc9471c75d9aa0581501825fc4d483c8
Shekhar Maharaj <shekharmaharaj2905@gmail.com>
2023-06-15 23:48:13 +0200

    Added form to book page | ToDo styling

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -5,46 +5,6 @@ import Footer from "../components/footer";
 export default function AboutUs(){
     return (
         <>
-            <section className="about-us">
-                <h1>About Our Little Slice of Heaven...</h1>
-                <div className="about-us-container">
-                    <div className="about-text">
-                        <p>
-                            Nestled serenely amidst majestic mountains and enveloped by enchanting woodlands, ParadiseBeach is a haven for wanderers seeking an idyllic retreat. This resort offers an array of thrilling adventures for both adrenaline junkies and those in search of a more laid-back experience. Embark on heart-pounding escapades like hiking, mountain biking, or zip-lining through the lush terrain. For those craving tranquility, indulge in leisurely walks amidst the captivating natural surroundings. Amidst it all, culinary delights await, promising a gastronomic journey that tantalizes the taste buds. From delectable local cuisine to international delights, ParadiseBeach ensures that every guest is pampered with exceptional flavors.
-                        </p><br/>
-                        <p>
-                            Nestled between the rugged grandeur of towering mountains and embraced by a tapestry of breathtaking woodlands, ParadiseBeach stands as a gateway to a world of exhilarating adventure and natural wonders. As you step onto the resort grounds, you are instantly immersed in a diverse and captivating environment. The surrounding terrains offer a myriad of biomes, each boasting its own unique charm and allure.
-                        </p><br/>
-
-                        <p>
-                            To the east, pristine sandy beaches stretch as far as the eye can see, caressed by the gentle lapping of turquoise waves. Here, guests can bask in the warmth of the sun, engage in thrilling water sports, or simply unwind on the shore, lulled by the rhythmic symphony of the ocean.
-                        </p><br/>
-                        <p>
-                            Venturing westward, the landscape transforms into a lush, verdant paradise. Towering ancient trees create a dense canopy overhead, filtering sunlight and casting enchanting patterns on the forest floor. Hiking trails wind their way through this emerald wilderness, beckoning adventurers to explore its hidden treasures. Discover cascading waterfalls, secret caves, and vibrant wildlife that call this vibrant ecosystem home.
-                        </p><br/>
-                        <p>
-                            As the terrain gradually ascends, the air becomes crisp and refreshing, revealing the majesty of towering mountains. Here, daredevils can test their limits with exhilarating rock climbing, rappelling down sheer cliffs, or conquering rugged peaks. The panoramic vistas from these lofty heights are nothing short of awe-inspiring, rewarding those who undertake the journey with breathtaking views of cascading valleys and distant horizons.
-                        </p><br/>
-                        <p>
-                            Beyond the mountains, vast stretches of untamed wilderness unfold, where untouched beauty reigns supreme. Dotted with crystal-clear lakes and meandering rivers, this pristine landscape invites visitors to engage in thrilling water adventures such as kayaking, white-water rafting, or fishing in tranquil solitude.</p><br/>
-                        <p>
-                            Back at the resort, adventure-seekers can find respite in luxurious accommodations that seamlessly blend with the surrounding nature. Whether it's a cozy log cabin nestled amidst the forest or an elegant villa overlooking the azure ocean, every abode provides a sanctuary of comfort and tranquility.
-                        </p><br/>
-                        <p>
-                            ParadiseBeach not only caters to the intrepid explorer but also ensures that each guest's palate is delighted with a culinary experience as diverse as the surrounding environment. From savoring delectable seafood delicacies harvested from nearby waters to indulging in farm-to-table delights showcasing the region's rich bounty, the resort's dining options promise to satisfy even the most discerning epicurean.
-                        </p><br/>
-                        <p>
-                            In this adventure seeker's haven, ParadiseBeach stands as an oasis of thrilling escapades and captivating natural beauty. Whether you seek the rush of adrenaline or the serenity of nature, this resort offers an unforgettable journey through diverse terrains and biomes, leaving you with memories to cherish long after your departure.
-                        </p><br/>
-
-
-                    </div>
-                    <div className="about-img">
-                        <img src="images/logo.svg" alt="logo"/>
-                    </div>
-                </div>
-            </section>
-
             <section className="about-section" id="beautiful-shores">
                 <div id="about-note-shores">
                     <div className="note-heading">About Our Little Slice of Heaven...</div>
--- src/pages/book.js
+++ src/pages/book.js
@@ -6,6 +6,9 @@ import { useParams } from "react-router-dom";
 export default function Book(){
 
     const params = useParams();
+    const handleBooking = ()=>{
+        alert("Thankyou for your interest in one of our elite rooms!Our bookings are currently full. Please try again later.");
+    }
 
     var showErrormesage = false;
     // In a perfect world, the following variables would be grabbed from a DB
@@ -14,6 +17,40 @@ export default function Book(){
     var roomLength;
     var roomPrice;
     var roomDescription;
+    var roomImage;
+    var terms_and_conditions = <div id="terms">
+ <h1>Terms and Conditions</h1>
+    <h2>Room Booking at Adventure Resort</h2>
+
+    <p>Welcome to Adventure Resort! These terms and conditions govern your booking of a resort room with us. Please read this document carefully before making a reservation.</p>
+
+    <p>1. Reservation and Confirmation</p>
+    <p>All room reservations are subject to availability. To book a room, you must provide accurate personal information and payment details. Upon successful reservation, you will receive a confirmation email containing the booking details.</p>
+
+    <p>2. Payment and Cancellation Policy</p>
+    <p>a) Payment: Full payment is required at the time of booking. We accept major credit cards and electronic bank transfers. Payment details will be provided during the reservation process.</p>
+    <p>b) Cancellation: Cancellations made at least 48 hours prior to the scheduled arrival date will receive a full refund. Cancellations made within 48 hours of the scheduled arrival date will incur a cancellation fee equivalent to one night's stay.</p>
+
+    <p>3. Check-In and Check-Out</p>
+    <p>a) Check-In: Standard check-in time is 3:00 PM local time. Early check-in is subject to availability and may incur additional charges.</p>
+    <p>b) Check-Out: Standard check-out time is 11:00 AM local time. Late check-out may be granted upon request, subject to availability and additional charges.</p>
+
+    <p>4. Guest Responsibility</p>
+    <p>a) Age Requirement: Guests must be 18 years of age or older to book a room and check-in.</p>
+    <p>b) Behavior: Guests are expected to conduct themselves in a respectful manner and comply with resort policies. Any damage caused to resort property will be the responsibility of the guest.</p>
+
+    <p>5. Liability</p>
+    <p>Adventure Resort is not liable for any loss, damage, or injury that occurs during your stay, including but not limited to personal belongings, accidents, or medical emergencies. Guests are encouraged to obtain travel insurance for their own protection.</p>
+
+    <p>6. Modifications and Termination</p>
+    <p>Adventure Resort reserves the right to modify or terminate any reservation or these terms and conditions at its discretion, with or without notice.</p>
+
+    <p>7. Governing Law</p>
+    <p>These terms and conditions are governed by and construed in accordance with the laws of the jurisdiction in which Adventure Resort is located.</p>
+
+    <p>By making a reservation, you acknowledge that you have read, understood, and agreed to these terms and conditions.</p>
+
+    </div>;
 
     switch (params.roomName) {
         case "room-1":
@@ -21,8 +58,11 @@ export default function Book(){
             roomWidth = 1300;
             roomLength = 1320;
             roomPrice = 4500;
+            roomImage = "/images/hotel-room1.jpg";
             roomDescription = <div id="room-description">
-                <p></p>
+                <p>
+                <span className="highlight">Experience the epitome of coastal luxury</span> in the Ocean Vista Suite, where breathtaking ocean views and modern elegance converge. Step into a spacious and sunlit room adorned with coastal-inspired decor, featuring soothing blue accents and natural textures. Sink into a plush king-sized bed with premium linens, as the sound of crashing waves lulls you into a blissful slumber. Enjoy a private balcony or terrace overlooking the sandy beach, perfect for savoring vibrant sunsets or enjoying morning coffee with the gentle sea breeze. The ensuite bathroom boasts a deep soaking tub and a rainfall shower, offering a serene oasis to unwind and refresh.
+                </p>
             </div>;
         break;
         case "room-2":
@@ -30,8 +70,11 @@ export default function Book(){
             roomWidth = 1300;
             roomLength = 1320;
             roomPrice = 4500;
+            roomImage = "/images/hotel-room2.jpg";
             roomDescription = <div id="room-description">
-                <p></p>
+                <p>
+                <span className="highlight">Indulge in the ultimate beachside retreat with the Beachfront Bungalow,</span> a private haven steps away from the sandy shores. Immerse yourself in the cozy and inviting atmosphere, where tropical hues and natural elements blend harmoniously. The bedroom welcomes you with a plush queen-sized bed, promising a restful night's sleep after a day filled with adventure. Unwind on your private veranda, complete with comfortable seating and panoramic views of the sparkling ocean. Let the sound of crashing waves and the scent of saltwater rejuvenate your senses, creating a truly idyllic beachfront escape.
+                </p>
             </div>;
         break;
         case "room-3":
@@ -39,8 +82,11 @@ export default function Book(){
             roomWidth = 1300;
             roomLength = 1320;
             roomPrice = 4500;
+            roomImage = "/images/hotel-room3.jpg";
             roomDescription = <div id="room-description">
-                <p></p>
+                <p>
+                <span className="highlight">Embrace the tropical paradise in the Palm Paradise Villa,</span> where luxury and beachside living intertwine seamlessly. Step into a spacious and elegantly furnished villa, exuding a contemporary coastal charm. The bedroom features a lavish king-sized bed adorned with soft linens, providing the utmost comfort after a day of thrilling adventures. The expansive living area boasts floor-to-ceiling windows, offering stunning views of swaying palm trees and turquoise waters. Retreat to your private outdoor terrace, complete with a plunge pool and comfortable loungers, where you can soak up the sun or enjoy a refreshing cocktail in utter tranquility.
+                </p>
             </div>;
         break;
 
@@ -49,8 +95,11 @@ export default function Book(){
             roomWidth = 1300;
             roomLength = 1320;
             roomPrice = 4500;
+            roomImage = "/images/hotel-room4.jpg";
             roomDescription = <div id="room-description">
-                <p></p>
+                <p>
+                <span className="highlight">Discover the perfect blend of comfort and coastal charm</span> in the Seaside Retreat Room. Decorated in soothing tones and adorned with beach-inspired accents, this inviting room offers a cozy sanctuary for tired adventurers. Sink into a plush queen-sized bed with crisp linens, ensuring a peaceful night's sleep. Open the curtains to reveal mesmerizing glimpses of the nearby beach and let the natural light flood the room. The ensuite bathroom features modern fixtures and a rejuvenating rain shower, providing a refreshing oasis after a day of outdoor activities. Whether you're lounging indoors or sipping cocktails on your private balcony, the Seaside Retreat Room offers a serene coastal escape.
+                </p>
             </div>;
         break;
         case "room-5":
@@ -58,8 +107,11 @@ export default function Book(){
             roomWidth = 1300;
             roomLength = 1320;
             roomPrice = 4500;
+            roomImage = "/images/hotel-room5.jpg";
             roomDescription = <div id="room-description">
-                <p></p>
+                <p>
+                <span className="highlight">Escape to a secluded paradise in the Tropical Oasis Cabana,</span> nestled amidst lush palm trees and vibrant flora. Step into a beautifully designed space that reflects the spirit of the tropics, with rattan furniture, vibrant patterns, and warm earthy tones. Relax on a comfortable king-sized bed with luxurious bedding, as gentle ocean breezes waft through the open windows. Unwind on your private patio, surrounded by a tropical garden, where you can lounge in a hammock or take a refreshing dip in your private plunge pool. The Tropical Oasis Cabana offers a true escape into a world of natural beauty and serenity.
+                </p>
             </div>;
         break;
         case "room-6":
@@ -67,8 +119,11 @@ export default function Book(){
             roomWidth = 1300;
             roomLength = 1320;
             roomPrice = 4500;
+            roomImage = "/images/hotel-room6.jpg";
             roomDescription = <div id="room-description">
-                <p></p>
+                <p>
+                <span className="highlight">Immerse yourself in coastal elegance in the Coastal Chic Suite,</span> where contemporary design and beachside vibes come together. Step into a stylishly furnished room featuring clean lines, neutral tones, and touches of nautical-inspired decor. Sink into a sumptuous king-sized bed and let the sound of the waves lull you into a restful sleep. Admire panoramic ocean views from your private balcony, furnished with comfortable seating for enjoying morning coffee or breathtaking sunsets. The ensuite bathroom exudes sophistication with modern fixtures, a spacious walk-in shower, and luxurious bath amenities. The Coastal Chic Suite is a perfect retreat for those seeking modern luxury in a beachfront setting.
+                </p>
             </div>;
         break;
     
@@ -82,21 +137,99 @@ export default function Book(){
     return (
         <>
             <section id="book-now-heading">
-                <h1>Book {params.roomName}</h1>
+                <h1>{params.roomName}</h1>
             </section>
             <section id="book-now-description">
-                {roomDescription}
-                <div id="room-dimensions">
-                    <ul>
-                        <li><em>Height: {roomHeight}</em></li>
-                        <li><em>Width: {roomWidth}</em></li>
-                        <li><em>Length: {roomLength}</em></li>
-                    </ul>
+                <div id="description-img">
+                    <img src={roomImage} alt="Room"/>
                 </div>
-                <div id="room-price-container">
-                    <span id="room-price">
-                        {roomPrice}
-                    </span>
+                <div id="description-detail">
+                    {roomDescription}
+                    <div id="room-dimensions">
+                        <span><em>Dimensions:</em></span>
+                        <ul>
+                            <li><em>Height: {roomHeight}</em></li>
+                            <li><em>Width: {roomWidth}</em></li>
+                            <li><em>Length: {roomLength}</em></li>
+                        </ul>
+                    </div>
+                    <div id="room-price-container">
+                        <span id="room-price">
+                            $ {roomPrice} Per Night
+                        </span>
+                    </div>
+                </div>
+            </section>
+            <section id="booking-details-container">
+                <div id="booking-details-terms">
+                    {terms_and_conditions}
+                    <div id="terms-accept-box">
+                        <label>Accept terms and conditions? </label>
+                        <input type="checkbox" id="terms"/>
+                    </div>
+                </div>
+                <div id="booking-details">
+                    <h1>Booking Details</h1>
+                    <div id="booking-form">
+                        <div className="form-input">
+                            <label>First Name:</label>
+                            <span className="input-validation-field">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M9 16.17l-4.17-4.17-1.42 1.41L9 19 21.59 6.41 20.17 5" fill="none" stroke="green" stroke-width="2" />
+                                </svg>
+                            </span>
+                            <input type="text" placeholder="John"/>
+                        </div>
+                        <div className="form-input">
+                            <label>Last Name:</label>
+                            <span className="input-validation-field">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M9 16.17l-4.17-4.17-1.42 1.41L9 19 21.59 6.41 20.17 5" fill="none" stroke="green" stroke-width="2" />
+                                </svg>
+                            </span>
+                            <input type="text" placeholder="Doe"/>
+                        </div>
+                        <div className="form-input">
+                            <label>Email Address:</label>
+                            <span className="input-validation-field">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M9 16.17l-4.17-4.17-1.42 1.41L9 19 21.59 6.41 20.17 5" fill="none" stroke="green" stroke-width="2" />
+                                </svg>
+                            </span>
+                            <input type="email" placeholder="john@example.com"/>
+                        </div>
+                        <div className="form-input">
+                            <label>Backup Email:</label>
+                            <span className="input-validation-field">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M9 16.17l-4.17-4.17-1.42 1.41L9 19 21.59 6.41 20.17 5" fill="none" stroke="green" stroke-width="2" />
+                                </svg>
+                            </span>
+                            <input type="email" placeholder="doe@example.com"/>
+                        </div>
+
+                        <div className="form-input">
+                            <label>From (Date):</label>
+                            <span className="input-validation-field">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M9 16.17l-4.17-4.17-1.42 1.41L9 19 21.59 6.41 20.17 5" fill="none" stroke="green" stroke-width="2" />
+                                </svg>
+                            </span>
+                            <input type="date"/>
+                        </div>
+                        <div className="form-input">
+                            <label>To (Date):</label>
+                            <span className="input-validation-field">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M9 16.17l-4.17-4.17-1.42 1.41L9 19 21.59 6.41 20.17 5" fill="none" stroke="green" stroke-width="2" />
+                                </svg>
+                            </span>
+                            <input type="date"/>
+                        </div>
+                        <div id="book-btn-container">
+                            <input type="button" id="book-btn" value="Book Now With PayPal" onClick={handleBooking}/>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
